01b98ba67bd4242345c3951933c3688155b0dd63
yehuda linker <yehuda.linker@gmail.com>
2026-01-22 10:30:49 +0200

    Creating a FASTAPI server and a SERVICE (which contains applications that operate on the data frame, add a column, and clean up missing data)

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import pandas as pd
+import json
+
+from servic import processing_data
+
+
+app = FastAPI()
+
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+    df = pd.read_csv(file.file)
+    df = processing_data(df)
+    return {"filename": df.info()}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
